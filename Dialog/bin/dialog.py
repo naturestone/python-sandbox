04ce3c146c2d2133011c3a3a8b619db425a7191d
naturestone <d8r3v8d3r@gmail.com>
2019-12-27 01:04:46 +0100

    Kommentare hinzugefügt

--- Dialog/bin/dialog.py
+++ Dialog/bin/dialog.py
@@ -25,17 +25,19 @@ class Application:
         self.data = data
 
     def main(self):
+        # Anzeigen des Programmnamens
         print("ID: {:s}".format(self.data["ID"]))
-        print("NAME: {:s}".format(self.data["NAME"]))
-        print("")
+        print("NAME: {:s}\n".format(self.data["NAME"]))
+        # Eingabeaufforderung anzeigen und auf Eingabe warten
         intend = input(self.data["ASK"] + " ")
-
+        # Eingabe verarbeiten
         for value in self.data["CHOOSE"]:
             if (value == intend):
                 say = self.data["IFOK"][random.randint(0,3)]
                 break
             else:
                 say = self.data["OTHERWISE"][random.randint(0,3)]
+        # Ausgabe des Ergebnisses
         print("\n{:s}\n".format(say))
         return 0
 
