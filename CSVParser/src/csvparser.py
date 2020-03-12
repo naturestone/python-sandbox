3114b7a843f9d39bcb7e4e2b27471133fc398e09
naturestone <d8r3v8d3r@gmail.com>
2020-03-13 00:21:43 +0100

    Initales hinzufügen

---
+++ CSVParser/src/csvparser.py
@@ -0,0 +1,92 @@
+#!/usr/bin/python3
+## -*- coding: utf-8 -*-
+
+# Daten für Dialog-Anwendung 
+AppData = {
+    "ID": "2020031123",
+    "TYPE": "Application",
+    "NAME": "CSV Parser",
+    "VERSION": "Version 0.1",
+    "INFILE": "../data/people.csv"
+}
+
+StatisticData = {
+
+}
+
+import os       # Filesystem Operationen
+import re       # Regular Expressions
+import csv      # CSV DictReader
+
+##########################################################################
+# Application Class (Dialog Application)
+
+class Application:
+
+    def __init__(self, data):
+        self.data = data
+
+    def main(self):
+        # Anzeigen des Programmnamens
+        print("{0:s}, {1:s} ({2:s})".format(self.data["NAME"], self.data["VERSION"], self.data["ID"]))
+
+    def dump(self, filename = ""):
+        load_file = self.data["INFILE"]
+        if filename != "":
+            load_file = filename
+        print("Load File: {0}\n".format(load_file))
+
+        # Öffne eine CSV-Datei
+        with open(load_file) as csvfile:
+            # Erzeuge den CSV-Reader
+            reader = csv.DictReader(csvfile)
+            count = 0
+            # Lade die CSF-Datei zeileweise 
+            for record in reader:
+                # Zerlege die Zeile in Spaltenname und Wert
+                for name,value in record.items():
+                    # Hole Schlüsselfeld 
+                    if name == "employeenumber":
+                        count+=1
+                        print("[{}]:".format(value))
+                        print("count={0}".format(count))
+                    print("{0}={1} ".format(name,value))      # without newline: end = " "
+                    try:
+                        # Hole pro Spalte ein vorhandenes Statistikfeld wenn vorhanden
+                        stdata = StatisticData[name]
+                    except KeyError:
+                        # Erzeuge pro Spalte ein Statistikfeld (initiales anlegen); unterscheide Typ String von Bool
+                        if name == 'active':
+                            StatisticData[name] = {'EMPTY':0, 'NOTEMPTY':0, 'ACTIVE': 0, 'INACTIVE': 0}     # Bool
+                        else:
+                            StatisticData[name] = {'EMPTY':0, 'NOTEMPTY':0}     # String (Standardtyp)
+
+                    # Prüfe und zähle hoch
+                    stdata = StatisticData[name]
+                    if value == "":
+                        stdata['EMPTY'] = stdata['EMPTY'] + 1
+                    if value != "":
+                        stdata['NOTEMPTY'] = stdata['NOTEMPTY'] + 1
+                    
+                    # bei bool-Typen: prüfe auf aktiv/inaktiv
+                    if name == 'active':
+                        if int(value) > 0:
+                            stdata['ACTIVE'] = stdata['ACTIVE'] + 1
+                        else:
+                            stdata['INACTIVE'] = stdata['INACTIVE'] + 1
+
+                # Eine Leerzeile pro Datenobjekt
+                print()
+
+        print(StatisticData)
+
+        print("\nCompleted: {} items.".format(count))
+        return 0
+        
+##########################################################################
+# MAIN Procedure
+
+if __name__ == "__main__":
+    App = Application(AppData)
+    App.main()
+    App.dump()
